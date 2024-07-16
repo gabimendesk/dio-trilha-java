5d1179fada0d8c8e52eff33ee020e54c5552cbdd
gabimendesk <gabidoth@gmail.com>
2024-07-15 23:35:44 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -7,21 +7,21 @@ public class ContaTerminal {
 
 	    public static void main(String[] args) {
 		
-		    int numero;
-		    String agencia, nomeCliente;
-		    double saldo;
+		int numero;
+		String agencia, nomeCliente;
+		double saldo;
 		
-		    System.out.println("Por favor, digite o número da Agência ! ");
+		System.out.println("Por favor, digite o número da Agência ! ");
 	    	agencia = ler.next();
-		    System.out.println("Por favor, digite o número da Conta ! ");
-		    numero = ler.nextInt();
-            System.out.println("Por favor, digite o seu Nome ! " );
-            nomeCliente = ler.next();
-            System.out.println("Por favor, digite o valor do seu saldo ! ");
-            saldo = ler.nextDouble();
+		System.out.println("Por favor, digite o número da Conta ! ");
+		numero = ler.nextInt();
+            	System.out.println("Por favor, digite o seu Nome ! " );
+           	 nomeCliente = ler.next();
+            	System.out.println("Por favor, digite o valor do seu saldo ! ");
+            	saldo = ler.nextDouble();
         
-            System.out.printf("\n Olá %s, obrigado por criar uma conta em nosso banco, ", nomeCliente);
-            System.out.printf("sua agência é %s, conta %s e seu saldo %s já está disponível para saque", agencia, numero, saldo);
-	}
-} 
+           	 System.out.printf("\n Olá %s, obrigado por criar uma conta em nosso banco, ", nomeCliente);
+            	System.out.printf("sua agência é %s, conta %s e seu saldo %s já está disponível para saque", agencia, numero, saldo);
+		}
+	} 
 
